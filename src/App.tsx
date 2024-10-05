36989173125ffd41df1755a6af51a79986a459f0
Roberto <p.roberto@wustl.edu>
2024-10-04 22:23:15 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,9 @@ function App() {
         <div className="App">
             <header className="App-header">
                 {/* <img src={} className="App-logo" alt="logo" /> */}
-
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    Edit <code>src/App.tsx</code> and save to reload.
                 </p>
-
                 <a
                     className="App-link"
                     href="https://reactjs.org"
